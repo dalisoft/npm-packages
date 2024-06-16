69fb810d7f35380a3379e677bbfd74f675dcdb48
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-06-16 13:35:22 +0500

    chore: formatting fixes and improvements

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import airlightNode from 'eslint-config-airlight-node';
+
+export default [
+  ...airlightNode,
+  {
+    name: 'npm-packages-root',
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off'
+    }
+  }
+];
